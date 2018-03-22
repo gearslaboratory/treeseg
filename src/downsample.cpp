f995f9914514c9e82f06c55a858ff792b1160328
Jonathan Greenberg <jgreenberg@unr.edu>
2018-03-22 13:19:11 -0700

    Calders' version of downsample.cpp

--- src/downsample.cpp
+++ src/downsample.cpp
@@ -1,50 +1,32 @@
+//Andrew Burt - a.burt.12@ucl.ac.uk
 
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
-#include <pcl/io/pcd_io.h>
+#include <treeseg.hpp>
 
-#include "treeseg.h"
-
-int main (int argc, char *argv[])
+int main (int argc,char** argv)
 {
-	float edgelength = atof(argv[1]);
-	bool catclouds = atoi(argv[2]);
-	pcl::PCDReader reader;
-	pcl::PCDWriter writer;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr original(new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr filtered(new pcl::PointCloud<pcl::PointXYZ>);
-	std::stringstream ss;
-	for(int i=3;i<argc;i++)
+	float tolerance = atof(argv[1]);
+	for(int i=2;i<argc;i++)
 	{
-		std::cout << "Reading plotcloud..." << std::endl;
-		reader.read(argv[i],*original);
-		if(catclouds == true)
-		{
-			pcl::PointCloud<pcl::PointXYZ>::Ptr tmp(new pcl::PointCloud<pcl::PointXYZ>);
-			std::cout << "Downsampling plotcloud..." << std::endl;
-			downsample(original,edgelength,tmp);
-			*filtered += *tmp;
-			original->clear();
-		}
-		if(catclouds == false)
-		{
-			std::vector<std::string> id = getFileID(argv[i]);
-			downsample(original,edgelength,filtered);
-			ss.str("");
-			ss << id[1] << "_" << id[0] << ".downsample.pcd"; 
-			writer.write(ss.str(),*filtered,true);
-			original->clear();	
-			filtered->clear();
-		}
-
-	}
-	if(catclouds == true)
-	{
-		std::vector<std::string> id = getFileID(argv[3]);
-		ss.str("");
-		ss << id[1] << ".downsample.pcd";
-		std::cout << "Writing downsamples plotcloud..." << std::endl;
-		writer.write(ss.str(),*filtered,true);
+		std::string fname;
+		std::vector<std::string> name1;
+		std::vector<std::string> name2;
+		std::vector<std::string> name3;
+		boost::split(name1,argv[i],boost::is_any_of("."));
+		boost::split(name2,name1[name1.size()-2],boost::is_any_of("/"));
+		//boost::split(name3,name2[name2.size()-1],boost::is_any_of("_"));
+		fname = name2[name2.size()-1];
+		std::stringstream ss;
+		ss << fname << ".downsample.pcd";
+		pcl::PCDReader reader;
+		pcl::PCDWriter writer;
+		pcl::VoxelGrid<pcl::PointXYZ> downsample;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_down(new pcl::PointCloud<pcl::PointXYZ>);
+		reader.read(argv[i],*cloud_in);
+		downsample.setInputCloud(cloud_in);
+		downsample.setLeafSize(tolerance,tolerance,tolerance);
+		downsample.filter(*cloud_down);
+		writer.write(ss.str(),*cloud_down,true);
 	}
 	return 0;
 }
